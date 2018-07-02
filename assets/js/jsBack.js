332585c18553f11d6756a6b9641bd1b38775f560
NadegeH <nadege@lacouleurduzebre.com>
2018-07-02 13:26:00 +0200

    création automatique des urls des catégories et des pages

--- assets/js/jsBack.js
+++ assets/js/jsBack.js
@@ -15,4 +15,43 @@ $(document).ready(function(){
     $('.action-save').click(function(){
         tinyMCE.triggerSave();
     });
+
+    /* URL automatique */
+    creationURL = function( event ){
+        if($('body').hasClass('new')) {
+            var caracteresInterdits = new RegExp('[ \'\"]', 'gi');
+            var e = new RegExp('[éèêëÉÈÊË]', 'gi');
+            var a = new RegExp('[àÀ]', 'gi');
+            var u = new RegExp('[ùûÛ]', 'u');
+            var o = new RegExp('[ôÔ]', 'u');
+            var i = new RegExp('[îïÎÏ]', 'i');
+            titreOK = $(this).val()
+                .replace(caracteresInterdits, '-')
+                .replace(e, 'e')
+                .replace(a, 'a')
+                .replace(u, 'u')
+                .replace(o, 'o')
+                .replace(i, 'i')
+                .toLowerCase();
+            url = encodeURIComponent(titreOK);
+            console.log(titreOK);
+            console.log(url);
+            $(event.data.cible).val(url);
+        }
+    };
+
+        /* Pour les pages */
+        $('#page_active_titre').on('keyup', {
+            cible: '#page_active_SEO_url'
+        }, creationURL );
+
+        /* Pour les catégories */
+        $('#categorie_nom').on('keyup', {
+            cible: '#categorie_url'
+        }, creationURL );
+
+        /* Pour les types de catégorie */
+        $('#typecategorie_nom').on('keyup', {
+            cible: '#typecategorie_url'
+        }, creationURL );
 });
