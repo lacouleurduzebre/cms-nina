9abf36da73e509dba9ce108102debe103903e055
Nadège HAMANN <nadege.hamann@hotmail.fr>
2018-07-18 11:06:30 +0200

    astérisque à côté des champs obligatoires

---
+++ src/Migrations/Version20180718085659.php
@@ -0,0 +1,28 @@
+<?php declare(strict_types = 1);
+
+namespace DoctrineMigrations;
+
+use Doctrine\DBAL\Migrations\AbstractMigration;
+use Doctrine\DBAL\Schema\Schema;
+
+/**
+ * Auto-generated Migration: Please modify to your needs!
+ */
+class Version20180718085659 extends AbstractMigration
+{
+    public function up(Schema $schema)
+    {
+        // this up() migration is auto-generated, please modify it to your needs
+        $this->abortIf($this->connection->getDatabasePlatform()->getName() !== 'mysql', 'Migration can only be executed safely on \'mysql\'.');
+
+        $this->addSql('ALTER TABLE nina_configuration ADD CONSTRAINT FK_B2A1BF5E968B03E4 FOREIGN KEY (langue_defaut_id) REFERENCES nina_langue (id)');
+    }
+
+    public function down(Schema $schema)
+    {
+        // this down() migration is auto-generated, please modify it to your needs
+        $this->abortIf($this->connection->getDatabasePlatform()->getName() !== 'mysql', 'Migration can only be executed safely on \'mysql\'.');
+
+        $this->addSql('ALTER TABLE nina_configuration DROP FOREIGN KEY FK_B2A1BF5E968B03E4');
+    }
+}
