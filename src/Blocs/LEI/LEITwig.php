ebe2daa96408c6718f9f12ad9db1581e8da52bf3
NadegeH <nadege@lacouleurduzebre.com>
2019-01-04 11:03:05 +0100

    édtion d'une page : catégories regroupées par type, bloc lei : limitation à un critère de moda

--- src/Blocs/LEI/LEITwig.php
+++ src/Blocs/LEI/LEITwig.php
@@ -28,10 +28,31 @@ class LEITwig extends \Twig_Extension
         );
     }
 
-    public function listeLEI($flux, $limite = null)
+    public function listeLEI($flux, $limite = null, $clef_moda = null)
     {
         $xml = simplexml_load_file($flux);
         $fiches = $xml->xpath("//Resultat/sit_liste");
+
+        if(isset($clef_moda)){
+            $resultat = [];
+
+            foreach($fiches as $fiche){
+                $criteres = $fiche->CRITERES->Crit;
+                foreach($criteres as $critere){
+                    $attribute = $critere->attributes()['CLEF_MODA'];
+                    if($attribute == $clef_moda){
+                        $resultat[] = $fiche;
+                        break;
+                    }
+                }
+            }
+
+            if(isset($limite)){
+                $resultat = array_splice($resultat, 0, $limite);
+            }
+
+            return $resultat;
+        }
 
         if(isset($limite)){
             $fiches = array_splice($fiches, 0, $limite);
