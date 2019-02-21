2bdb15f9b39d63703aa3d6f2ed299fe0dfd4e8e2
NadegeH <nadege@lacouleurduzebre.com>
2019-02-21 14:27:34 +0100

    rss (3)

--- src/Controller/RSSController.php
+++ src/Controller/RSSController.php
@@ -9,6 +9,7 @@
 namespace App\Controller;
 
 
+use App\Entity\BlocAnnexe;
 use App\Entity\Langue;
 use App\Entity\Page;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
@@ -18,7 +19,7 @@ use Symfony\Component\Routing\Annotation\Route;
 class RSSController extends Controller
 {
     /**
-     * @Route("/{_locale}/rss.xml", defaults={"_format"="xml"}, name="rss")
+     * @Route("/{_locale}/rss.xml", name="rss")
      * @param Request $request
      * @return \Symfony\Component\HttpFoundation\RedirectResponse|\Symfony\Component\HttpFoundation\Response
      */
@@ -29,6 +30,20 @@ class RSSController extends Controller
         $repoPage = $this->getDoctrine()->getRepository(Page::class);
         $pages = $repoPage->pagesPublieesCategorie(0, $langue, 10);
 
-        return $this->render('front/rss.xml.twig', array('pages' => $pages));
+        $repoBlocAnnexe = $this->getDoctrine()->getRepository(BlocAnnexe::class);
+        $vignettes = [];
+        foreach($pages as $page){
+            $vignette = $repoBlocAnnexe->findOneBy(array('page' => $page, 'type' => 'Vignette'));
+
+            if($vignette){
+                $image = $this->getParameter('kernel.project_dir').'/public'.$vignette->getContenu()['image']['image'];
+                if(is_file($image)){
+                    $vignettes[$page->getId()]['taille'] = filesize($image);
+                    $vignettes[$page->getId()]['type'] = filetype($image);
+                }
+            }
+        }
+
+        return $this->render('front/rss.xml.twig', array('pages' => $pages, 'vignettes' => $vignettes));
     }
 }
