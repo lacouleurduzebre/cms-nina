25d06fe9259aafba2350d4c0900a6413af16f25d
Nadège Thierry <nadege@lacouleurduzebre.com>
2019-08-21 10:15:58 +0200

    entité rôle, fichier de config, route

---
+++ src/Controller/Back/DroitsController.php
@@ -0,0 +1,40 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: nadege
+ * Date: 2019-08-21
+ * Time: 09:36
+ */
+
+namespace App\Controller\Back;
+
+use App\Entity\Role;
+use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\Yaml\Yaml;
+
+/**
+ * Class DroitsController
+ * @package App\Controller\Back
+ * @Route("/admin")
+ */
+class DroitsController extends Controller
+{
+    /**
+     * @Route("/droits", name="droits")
+     * @param Request $request
+     * @return bool|Response
+     */
+    public function listeDroits(){
+        //Rôles
+        $repoRoles = $this->getDoctrine()->getRepository(Role::class);
+        $roles = $repoRoles->findAll();
+
+        //Droits
+        $droits = Yaml::parsefile('../config/droits.yaml');
+
+        return $this->render('back/droits.html.twig', ['roles' => $roles, 'droits' => $droits]);
+    }
+}
---
+++ src/Entity/Role.php
@@ -0,0 +1,62 @@
+<?php
+
+namespace App\Entity;
+
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ * @ORM\Entity(repositoryClass="App\Repository\RoleRepository")
+ */
+class Role
+{
+    /**
+     * @ORM\Id()
+     * @ORM\GeneratedValue()
+     * @ORM\Column(type="integer")
+     */
+    private $id;
+
+    /**
+     * @ORM\Column(type="string", length=255)
+     */
+    private $nom;
+
+    /**
+     * @ORM\Column(type="array", nullable=true)
+     */
+    private $droits;
+
+    public function __toString()
+    {
+        return $this->nom;
+    }
+
+    public function getId()
+    {
+        return $this->id;
+    }
+
+    public function getNom(): ?string
+    {
+        return $this->nom;
+    }
+
+    public function setNom(string $nom): self
+    {
+        $this->nom = $nom;
+
+        return $this;
+    }
+
+    public function getDroits(): ?array
+    {
+        return $this->droits;
+    }
+
+    public function setDroits(?array $droits): self
+    {
+        $this->droits = $droits;
+
+        return $this;
+    }
+}
