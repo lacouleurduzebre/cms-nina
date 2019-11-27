aa01179cb9bc423da2789f69c1ce1b83df5a7774
Nadège Thierry <nadege@lacouleurduzebre.com>
2019-11-27 09:30:04 +0100

    tinymce : coller en tant que texte brut

--- public/assets/js/optionsTinyMCE.js
+++ public/assets/js/optionsTinyMCE.js
@@ -17,11 +17,13 @@ $(document).ready(function(){
         external_filemanager_path:"/filemanager/",
         external_plugins: { "filemanager" : "/filemanager/plugin.min.js"},
 
+        paste_as_text: true,
+
         extended_valid_elements: 'i[class=class]',
 
         block_formats: 'Paragraphe=p;Titre h2=h2;Titre h3=h3;Titre h4=h4;Titre h5=h5;Titre h6=h6',
         image_advtab: true,
-        toolbar1: "bold italic underline | link unlink | bullist numlist | alignleft aligncenter alignright",
+        toolbar1: "bold italic underline | link unlink | bullist numlist | alignleft aligncenter alignright | pastetext",
 
         init_instance_callback: function (editor) {
             editor.on('Change', function (e) {
@@ -65,6 +67,8 @@ $(document).ready(function(){
         filemanager_title:"Médiathèque",
         external_filemanager_path:"/filemanager/",
         external_plugins: { "filemanager" : "/filemanager/plugin.min.js"},
+
+        paste_as_text: true,
 
         extended_valid_elements: 'i[class=class]',
 
