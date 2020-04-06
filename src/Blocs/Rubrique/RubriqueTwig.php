c6e82195db5d77376fd873e37fb6d5780bf0b0ec
Nadège Thierry <nadege@lacouleurduzebre.com>
2020-04-06 14:36:38 +0200

    bloc rubrique : modif fonction twig (choix du menu de référence)

--- src/Blocs/Rubrique/RubriqueTwig.php
+++ src/Blocs/Rubrique/RubriqueTwig.php
@@ -11,13 +11,17 @@ namespace App\Blocs\Rubrique;
 
 use App\Entity\Menu;
 use App\Entity\MenuPage;
-use App\Entity\Bloc;
 use App\Service\Page;
 use App\Service\Pagination;
 use Doctrine\Persistence\ManagerRegistry;
+use Twig\Extension\AbstractExtension;
+use Twig\TwigFunction;
 
-class RubriqueTwig extends \Twig_Extension
+class RubriqueTwig extends AbstractExtension
 {
+    private $pagination;
+    private $doctrine;
+
     public function __construct(ManagerRegistry $doctrine, Pagination $pagination)
     {
         $this->doctrine = $doctrine;
@@ -27,28 +31,34 @@ class RubriqueTwig extends \Twig_Extension
     public function getFunctions()
     {
         return array(
-            new \Twig_SimpleFunction('listePagesEnfants', array($this, 'listePagesEnfants')),
+            new TwigFunction('listePagesEnfants', array($this, 'listePagesEnfants')),
         );
     }
 
     public function listePagesEnfants($pageParent, $parametres)
     {
         $repoMenu = $this->doctrine->getRepository(Menu::class);
-        $menuPrincipal = $repoMenu->findOneBy(array('defaut' => true, 'langue' => $pageParent->getLangue()));
-
         $repoMenuPage = $this->doctrine->getRepository(MenuPage::class);
 
-        $menuPage = $repoMenuPage->findOneBy(array('page' => $pageParent, 'menu' => $menuPrincipal));
+        $menusPages = $repoMenuPage->findBy(['page' => $pageParent]);
+
+        if(count($menusPages) == 1){//Si la page n'est que dans un menu
+            $menuPage = $menusPages[0];
+        }else{//Si la page est dans plusieurs menus, on prend le menu par défaut
+            $menu = $repoMenu->findOneBy(array('defaut' => true, 'langue' => $pageParent->getLangue()));
+            $menuPage = $repoMenuPage->findOneBy(array('page' => $pageParent, 'menu' => $menu));
+            if(!$menuPage){//Si la page n'est pas dans le menu par défaut, on prend le premier menuPage
+                $menuPage = $menusPages[0];
+            }
+        }
 
         $pages = [];
-        if($menuPage){
-            $menusPagesEnfants = $repoMenuPage->findBy(array('parent' => $menuPage), array('position' => 'ASC'));
-            foreach($menusPagesEnfants as $menuPageEnfant){
-                $pageEnfant = $menuPageEnfant->getPage();
+        $menusPagesEnfants = $repoMenuPage->findBy(array('parent' => $menuPage), array('position' => 'ASC'));
+        foreach($menusPagesEnfants as $menuPageEnfant){
+            $pageEnfant = $menuPageEnfant->getPage();
 
-                if(Page::isPublie($pageEnfant)){
-                    $pages[] = $pageEnfant;
-                }
+            if(Page::isPublie($pageEnfant)){
+                $pages[] = $pageEnfant;
             }
         }
 
