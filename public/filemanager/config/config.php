2ac4a75ad353a659b9574529adae5e71b6a169f3
Nadège Thierry <nadege@lacouleurduzebre.com>
2020-08-14 13:02:20 +0200

    fix upload images jpg, réduction du poids des images uploadées

--- public/filemanager/config/config.php
+++ public/filemanager/config/config.php
@@ -292,7 +292,7 @@ $config = array(
     // If you set $image_resizing to TRUE the script converts all uploaded images exactly to image_resizing_width x image_resizing_height dimension
     // If you set width or height to 0 the script automatically calculates the other dimension
     // Is possible that if you upload very big images the script not work to overcome this increase the php configuration of memory and time limit
-    'image_resizing'                          => false,
+    'image_resizing'                          => true,
     'image_resizing_width'                    => 0,
     'image_resizing_height'                   => 0,
     'image_resizing_mode'                     => 'auto', // same as $image_max_mode
